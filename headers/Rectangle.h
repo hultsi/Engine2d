f9b29a3bf37f846c5a47d4a9cbc6a143eddbf70f
Miro <miroruopsa@gmail.com>
2021-09-06 01:10:36 +0300

    rigidbody parent class implemented and rectangle extends that

--- headers/Rectangle.h
+++ headers/Rectangle.h
@@ -2,9 +2,10 @@
 #include <array>
 #include <string>
 #include "Vector2d.h"
+#include "RigidBody.h"
 
 namespace Engine2d {
-	class Rectangle {
+	class Rectangle : public RigidBody {
 	public:
 		std::string name;
 		std::array<Vector2d, 4> P;
@@ -17,12 +18,11 @@ namespace Engine2d {
 		Rectangle() {};
 		Rectangle(const float x, const float y, const float width, const float height, const float theta,
 				const bool isStatic, const float mass, std::string name);
+		~Rectangle() {};
 		void updatePosition();
 		void update();
 		void draw() const;
 		void operator=(Rectangle other);
-		// Debug
-		// void dbgDrawVelocityFromCorners() const;
 	private:
 		float width, height;
 		float centerAngle_1; // calculated with lines going from CM to corner points
---
+++ headers/RigidBody.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <array>
+#include <string>
+#include "Vector2d.h"
+
+namespace Engine2d {
+	class RigidBody {
+	public:
+		std::string name;
+		Vector2d position; // CM position
+		Vector2d dx;
+		float theta;
+		float dtheta;
+		bool collision;
+
+		RigidBody() {};
+		virtual ~RigidBody() {};
+		virtual void updatePosition() {};
+		virtual void update() {};
+		virtual void draw() const {};
+		virtual void operator=(RigidBody other) {};
+	protected:
+		float mass;
+		float invMass;
+		bool isStatic;
+	};
+}
